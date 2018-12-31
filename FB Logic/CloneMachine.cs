67bad22d63b4850751fe6dc3f15dff569022f712
Nir Yakov <Niry27@gmail.com>
2018-12-31 18:22:35 +0200

    Wierd

---
+++ FB Logic/CloneMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FB_Logic
+{
+    public static class CloneMachine
+    {
+        public static T DeepClone<T>(this T i_ToClone)
+            where T : class
+        {
+            using (Stream stream = new MemoryStream())
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+                serializer.Serialize(stream, i_ToClone);
+                stream.Flush();
+                stream.Seek(0, SeekOrigin.Begin);
+                T theClone = serializer.Deserialize(stream) as T;
+                return theClone;
+            }
+        }
+    }
+}
